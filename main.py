d3991b4131e1633b8787f477c6e855080a33c35d
Laky730 <benibubu2@gmail.com>
2021-05-21 17:23:58 +0200

    2nd task finished, and I uploaded pyfiglet too!

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pyfiglet as pyfiglet
 
 
+
+
+#Printing out a sequence from a famouse Hungarian poem.
+task1 = pyfiglet.figlet_format("TASK  1")
+
 Toldi = """TOLDI \n
 ELSŐ ÉNEK. \n \n Ég a napmelegtől a kopár szík sarja,
 Tikkadt szöcskenyájak legelésznek rajta;
@@ -11,6 +16,18 @@ Hortyog, mintha legjobb rendin menne dolga;
 Hej, pedig üresen, vagy félig rakottan,
 Nagy szénás szekerek álldogálnak ottan."""
 
-logiPython = pyfiglet.figlet_format("1. FELADAT")
-print(logiPython , "\n")
-print(Toldi)
+print(task1 , "\n")
+print(Toldi, "\n \n")
+
+#Calculating the volume of a cone.
+task2 = pyfiglet.figlet_format("TASK  2")
+print(task2 , "\n")
+
+radius = int(input("Radius (units): "))
+print("\n")
+height = int(input("Height (units): "))
+print("\n \n")
+volume = (radius ** 2 * 3.14) * height / 3
+
+print("The volume of this cone is ", volume, " units.")
+
