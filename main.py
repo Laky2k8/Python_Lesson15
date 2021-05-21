74123e7bd2339e3a505065a98a67a1f4fb0c5d70
Laky730 <benibubu2@gmail.com>
2021-05-21 17:10:43 +0200

    1st task finished, works pretty well :D

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("1.FELADAT \n \n")
-pog = input("Pog: \n \n")
+import pyfiglet as pyfiglet
+
+
+Toldi = """TOLDI \n
+ELSŐ ÉNEK. \n \n Ég a napmelegtől a kopár szík sarja,
+Tikkadt szöcskenyájak legelésznek rajta;
+Nincs egy árva fűszál a tors közt kelőben,
+Nincs tenyérnyi zöld hely nagy határ mezőben.
+Boglyák hűvösében tíz-tizenkét szolga
+Hortyog, mintha legjobb rendin menne dolga;
+Hej, pedig üresen, vagy félig rakottan,
+Nagy szénás szekerek álldogálnak ottan."""
+
+logiPython = pyfiglet.figlet_format("1. FELADAT")
+print(logiPython , "\n")
+print(Toldi)
